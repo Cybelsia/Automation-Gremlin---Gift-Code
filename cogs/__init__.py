bb616525f69ec91345db1c45c47595e06440b7e7
Cybelsia <Cybelsia@proton.me>
2026-04-17 00:21:16 +1000

    Add __init__.py for cogs folder

---
+++ cogs/__init__.py
@@ -0,0 +1,5 @@
+# Cogs initialization file
+
+# This file is used to initialize the cogs folder for the Automation-Gremlin project.
+
+# Update this file as necessary to include relevant information about cogs.
